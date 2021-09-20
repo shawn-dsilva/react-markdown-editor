dfbaced129f66b92816c0c71abb30d0d8cbb320f
Shawn Dsilva <shawn@shawndsilva.com>
2021-09-20 16:23:29 +0530

    text bold works

--- src/components/WriteDiv.js
+++ src/components/WriteDiv.js
@@ -1,100 +1,12 @@
 import React, { useContext, useRef, useState } from 'react';
 import { store } from '../store';
-import sanitizeHtml from 'sanitize-html';
 
-const ContentEditableWithRef = (props) => {
-    const defaultValue = useRef(props.value);
-  
-    const handleInput = (event) => {
-
-      if (props.onChange) {
-        props.onChange(event.target.innerHTML);
-        const text = event.target.innerHTML
-        let noHTMLData = text.replace(/(<([^>]+)>)/gi, "  \n");
-        console.log(noHTMLData);
-        props.dispatch({type:"text", data: noHTMLData});
-      }
-    };
-
-
-
-
-
-  
-    return (
-      <div className="writeDivContainer">
- <div className="writeBanner">EDIT
- <button onClick={props.textTransform}>B</button>
- </div>
-
-<span
-   className="writeDiv"
-  contentEditable
-  onInput={handleInput}
-  dangerouslySetInnerHTML={{ __html: defaultValue.current }}
-/>
-      </div>
-     
-    );
-  };
-  
 function WriteDiv() {
     const globalState = useContext(store);
     const { dispatch } = globalState;
     const [nameFromRef, setNameFromRef] = useState(``);
+    let textArea = useRef(null);
 
-    const makeBold = () => {
-      // const selection = window.getSelection();
-      // let {anchorNode, anchorOffset, focusNode, focusOffset} = selection;
-      // console.log(selection.toString());
-      // var range = selection.getRangeAt(0);
-      // var start = range.startOffset;
-      // var end = range.endOffset;
-      // console.log(`Sel Start ${anchorNode.data}, offset ${anchorOffset}`);
-      // console.log(`Sel End ${focusNode.data}, offset ${focusOffset}`);
-      // const bolded =  "**"+selection+"**";    
-      // let start = nameFromRef.substring(anchorOffset);
-      // let end = focusNode.data.substring(focusOffset);
-      // let para = start + bolded + end;
-      // console.log(anchorNode);
-      // console.log(focusNode);
-      // console.log(selection.toString());
-      // console.log(para);
-      // dispatch({type:"text", data: para});
-
-      
-    var offset = 0;
-    var selection = window.getSelection();
-    var range = selection.getRangeAt(0);
-    var start = range.startOffset;
-    var end = range.endOffset;
-
-    if ( selection.baseNode.parentNode.hasChildNodes() ) { 
-        for ( var i = 0 ; selection.baseNode.parentNode.childNodes.length > i ; i++ ) { 
-            var cnode = selection.baseNode.parentNode.childNodes[i];
-            if (cnode.nodeType == document.TEXT_NODE) {
-                if ( (offset + cnode.length) > start) {
-                    break;
-                }   
-                offset = offset + cnode.length;
-            }   
-            if (cnode.nodeType == document.ELEMENT_NODE) {
-                if ( (offset + cnode.textContent.length) > start) {
-                    break;
-                }   
-                offset = offset + cnode.textContent.length;
-            }   
-        }   
-    }   
-
-    start = start + offset;
-    end = end + offset;
-
-    console.log(selection.baseNode.parentNode.childNodes);
-
-    console.log("START: "+start);
-    console.log("END: "+end);
-    }
 
     const onChange = (event) => {
       let text = event.target.value
@@ -106,11 +18,27 @@ function WriteDiv() {
     }
 
 
+    const clickHandler = () => {
+      textArea = textArea.current;
+      if (textArea.selectionStart === textArea.selectionEnd) {
+        return; // nothing is selected
+      }
+    
+      let selected = textArea.value.slice(textArea.selectionStart, textArea.selectionEnd);
+      textArea.setRangeText(`**${selected}**`);
+      console.log(`${textArea.value}`);
+      setNameFromRef(textArea.value);
+      dispatch({type:"text", data: textArea.value});
+
+
+    }
+
     return (
       <div className="writeDivContainer">
       <div className="writeBanner">EDIT
+      <button onClick={clickHandler}>B</button>
       </div>
-        <textarea className="writeDiv" value={nameFromRef}  onChange={onChange}   />
+        <textarea ref={textArea} className="writeDiv" value={nameFromRef}  onChange={onChange}   />
       </div>
     )
 }
