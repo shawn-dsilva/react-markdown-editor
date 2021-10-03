11faa4ae00e13550c0f7058a2d9853517e53d8ac
Shawn Dsilva <shawn@shawndsilva.com>
2021-10-03 16:07:39 +0530

    download file logic improved

--- src/components/DownloadFile.js
+++ src/components/DownloadFile.js
@@ -4,23 +4,51 @@ import { store } from '../store';
 
 function DownloadFile(props) {
     
-    const clickHandler = (event) => {
-        event.preventDefault()
+    let dofileDownload = useRef(null);
+    const [fileDownloadUrl, setFileDownloadUrl] = useState(null);
+
+    useEffect(() => { 
+        dofileDownload.current.dispatchEvent(
+            new MouseEvent('click', {
+              view: window,
+              bubbles: true,
+              cancelable: true,
+              buttons: 1,
+            }),
+          );
+        console.log(fileDownloadUrl)  
+         URL.revokeObjectURL(fileDownloadUrl);   
+         setFileDownloadUrl(null);
+     }, [fileDownloadUrl])
+
+
+    function clickHandler() {
         localStorage.setItem('document', props.value);
         let file = localStorage.getItem("document");
         const blob = new Blob([file]);  
-        const element = document.createElement("a");
-        element.href = URL.createObjectURL(blob);
-        element.download = "readme.md";
-        document.body.appendChild(element);
-        element.click();
+        setFileDownloadUrl(URL.createObjectURL(blob));
+
+        // console.log(blob);                 // Step 3
+        // setFileDownloadUrl(URL.createObjectURL(blob));
+        // dofileDownload.click();                   // Step 6
+        // URL.revokeObjectURL(fileDownloadUrl);          // Step 7
+        // const element = document.createElement("a");
+        // element.href = URL.createObjectURL(blob);
+        // element.download = "readme.md";
+        // document.body.appendChild(element);
+        // element.click();
 
     }
 
     return (
         <div className="div-container">
-     <button onClick={ (event) => clickHandler(event)}>
+     <button onClick={clickHandler}>
              <i class="fas fa-file-download"></i>
+    <a style={{"display":"none"}}
+         download="readme.md"
+         href={fileDownloadUrl}
+         ref={dofileDownload}>
+        </a>
     </button>
         </div>
     )
